51d65097359f09a0fb6108a14ab00d7a0f6c5ca4
savioramon <savioramon12@gmail.com>
2021-07-19 14:43:13 -0300

    requisição completa

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,13 +1,19 @@
-import { all, put, takeEvery } from "redux-saga/effects";
+import { all, put, call, takeEvery } from "redux-saga/effects";
 
+import quizesRequest from "./quizesRequest";
+
+function* requisicao(){
+
+    const dadosApi = yield call(quizesRequest);
+
+    yield put({
+        type: "API_DADOS",
+        payload: dadosApi
+    })
+}
 
 export default function* sagas(){
     yield all([
-        takeEvery("API_DADOS_REQUEST", function*(action){
-            yield put({
-                type: "API_DADOS",
-                payload: action.payload
-            })
-        })
+        takeEvery("API_DADOS_REQUEST", requisicao)
     ])
 }
