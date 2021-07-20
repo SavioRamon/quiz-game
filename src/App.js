6e173ec79e94b5c12dcadd3d65288ba88b2cfe62
savioramon <savioramon12@gmail.com>
2021-07-19 21:43:47 -0300

    adicionando cabeçalho e o estilo dele

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from "react";
 import './App.css';
 
 function App() {
   	return (
  		<div className="App">
-      		...
+			 <header className="cabecalho">
+				 <h1 className="cabecalho-titulo">Quiz Game</h1>
+			 </header>
     	</div>
   	);
 }
