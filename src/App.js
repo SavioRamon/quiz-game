8dcb2f511258b9ecef495f9c64be95391bdc12a0
savioramon <savioramon12@gmail.com>
2021-07-22 10:25:13 -0300

    chamando a API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 
+import QuizesArea from "./componentes/QuizesArea";
+
+import recebeDados from "./store/quizApiDados/quizApiDados.actions";
+import { useDispatch } from "react-redux";
+
 function App() {
+
+	const dispatch = useDispatch();
+
+	useEffect(()=>{
+		dispatch(recebeDados());
+	}, [])
+
   	return (
  		<div className="App">
 			 <header className="cabecalho">
 				 <h1 className="cabecalho-titulo">Quiz Game</h1>
 				 
 			 </header>
+
+			<QuizesArea />
+
     	</div>
   	);
 }
