f991f3f57d67d1f916faf3a32dc7238693aa6271
savioramon <savioramon12@gmail.com>
2021-07-23 21:48:43 -0300

    adicionando reducer de informações à store

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 
 import reducerApi from "./quizApiDados/quizApiDados.reducer";
+import quizInformacoes from "./quizAreaPrincipal/quizAreaPrincipal.reducer";
 
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas/index";
 
 
 const rootReducer = combineReducers({
-    reducerApi
+    reducerApi,
+    quizInformacoes
 });
 
 const sagaMiddleware = createSagaMiddleware();
