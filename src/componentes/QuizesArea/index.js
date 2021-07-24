a10a894b8405a3f50d103f7ad86fd938f9fe15a0
savioramon <savioramon12@gmail.com>
2021-07-23 21:45:01 -0300

    consumindo a api

--- src/componentes/QuizesArea/index.js
+++ src/componentes/QuizesArea/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./style.css";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { informacoes } from "../../store/quizAreaPrincipal/quizAreaPrincipal.actions";
 
 function QuizesArea(){
 
-    const quizesApi = useSelector(state=>state.reducerApi)
+    const quizesApi = useSelector(state=>state.reducerApi);
+    const dispatch = useDispatch();
     return (
         <div className="quizes-selecao">
             {Array.isArray(quizesApi) &&
                 quizesApi.map((quiz, key)=>{
                     return(
-                        <div className="quiz-unidade" style={{backgroundImage: `url(${quiz.image})`}} key={key}>
+                        <div className="quiz-unidade" 
+                          style={{backgroundImage: `url(${quiz.image})`}} 
+                          key={key}
+                          onClick={()=>{
+                            dispatch(informacoes(quiz))
+                          }}
+                        >
                             
                             <h1>{quiz.title}</h1>
                             
