2a4670b39421d96bfa7722c09e493c640e40cd02
savioramon <savioramon12@gmail.com>
2021-07-27 18:10:20 -0300

    adicionando funcionalidade de iniciação do quiz

--- src/componentes/QuizPrincipal/index.js
+++ src/componentes/QuizPrincipal/index.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 import { useSelector } from "react-redux";
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 function QuizPrincipal(){
 
     const quizInfo = useSelector(state=>state.quizInformacoes);
-    console.log(quizInfo);
-    
+    const [quizAcoes, setQuizAcoes] = useState({
+        start: false,
+        restart: false
+    })
+
+    useEffect(()=>console.log(quizAcoes), [quizAcoes])
+
     return (
 
         <div className="quiz-principal-conteudo" style={{backgroundImage: `url(${quizInfo.image})`}}>
-            { quizInfo.estado === "inicial" ?
-                
+            
+            { quizInfo.estado === "inicial"?
+            
                 <div className="info-inicial">
                     <h1>{quizInfo.titulo}</h1>
                     <p>{quizInfo.texto}</p>
@@ -20,11 +27,18 @@ function QuizPrincipal(){
 
                 :
 
-                <div className="quiz-iniciado">
-                    
+                quizAcoes.start?
+                    <div className="quiz-iniciado">
+                        <h1 className="quiz-titulo">{quizInfo.title}</h1>
+                    </div>
 
-                </div>
+                    :
+                    
+                    <div className="start" onClick={()=>setQuizAcoes({...quizAcoes, start: true})}>
+                        <PlayArrowIcon style={{fontSize: "100px"}}/>
+                    </div>
             }
+
 
             
         </div>
